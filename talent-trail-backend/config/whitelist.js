a056d4685d58c13eba5ac07ac16fbe828e6e5fb9
Sai Sri Harsha Rankela <saisriharsha.r@gmail.com>
2026-03-26 09:10:43 +0530

    added configs for dbconn and whitelist and upload media

---
+++ talent-trail-backend/config/whitelist.js
@@ -0,0 +1,10 @@
+const whitelist = [
+    'https://www.google.com',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'https://final-year-project-client-five.vercel.app',
+    'http://ec2-52-206-17-63.compute-1.amazonaws.com:5000',
+    'http://localhost:5000'
+];
+
+module.exports = whitelist;
