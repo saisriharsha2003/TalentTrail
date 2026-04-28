506e61b3d9869f58b6fe1aa19d7a78b53f684ad1
Sai Sri Harsha Rankela <saisriharsha.r@gmail.com>
2026-04-28 17:13:34 +0530

    updated config file

--- talent-trail-frontend/src/api/config.js
+++ talent-trail-frontend/src/api/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:3500';
-// export const BASE_URL = 'https://talentrail-6ti6.onrender.com/';
+// export const BASE_URL = 'http://localhost:3500';
+export const BASE_URL = 'https://talentrail-6ti6.onrender.com/';
