63de2f882713a94ea9b2ff8dd2a2c050c3984e35
Sai Sri Harsha Rankela <saisriharsha.r@gmail.com>
2026-05-05 16:10:18 +0530

    updated index.js to correctly stores files

--- talentrail-backend/index.js
+++ talentrail-backend/index.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/resumes', express.static('resumes'));
+app.use('/uploads', express.static('uploads'));
 
 app.use('/', require('./routes/user'));
 app.use('/public', require('./routes/public'));
